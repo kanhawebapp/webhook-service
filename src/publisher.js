6c0e41cc16d071d5c0300b5a614ee7c2e757f054
tejpal singh <tej@dhwaniastro.com>
2026-09-05 17:49:56 +0530

    change for logs

--- src/publisher.js
+++ src/publisher.js
@@ -52,7 +52,7 @@ app.post("/webhook", async (req, res) => {
     return res.status(400).json({ error: "Invalid signature" });
   }
   console.log("Received webhook event:", req.body.event, "boddy:", req.body);
-  console.log("payload----from payment gatway-----------:", req.body.payload);
+  console.log("payload----from payment gatway----test-------:", req.body.payload);
   // Only handle payment captured
   if (
     req.body.event === "payment.captured" ||
